1d31f30b67b39bb7743f4be086405618014b1307
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-06-04 22:27:33 +0545

    i am puja

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard.index');
+    return view ('admin.dashboard.index');
 });
 
 Route::get('/', function () {
     return view('front.index');
 });
-//edited by puja 
+// final changes by pooja
